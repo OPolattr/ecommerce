2a35963440366376a5865d1fe7f30dc46ec437e6
Omer Polat <o.polat.tr@outlook.com>
2024-03-13 01:56:10 +0300

    first commit

---
+++ Store/StoreApp/Program.cs
@@ -0,0 +1,10 @@
+//web uygulamasının insa edilmesi
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/btk", () => "BTK Akademi!");
+
+app.Run();
+
+// \ {} []
